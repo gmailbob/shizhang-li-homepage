e7545237d81890c739f4d27cef967c5601eda79b
gmailbob <mianyangbob@gmail.com>
2019-11-05 01:27:08 -0500

    Updates

---
+++ precache-manifest.ebd38d11dfac05e07ee655707e2a4a7e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "542eee33a8962b8079e01f95a97cd351",
+    "url": "/shizhang-li-homepage/index.html"
+  },
+  {
+    "revision": "38a3bc43f72768be4630",
+    "url": "/shizhang-li-homepage/static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "00faee7f393fe60300c0",
+    "url": "/shizhang-li-homepage/static/css/main.ec6ab049.chunk.css"
+  },
+  {
+    "revision": "38a3bc43f72768be4630",
+    "url": "/shizhang-li-homepage/static/js/2.df052f62.chunk.js"
+  },
+  {
+    "revision": "00faee7f393fe60300c0",
+    "url": "/shizhang-li-homepage/static/js/main.2c553cf1.chunk.js"
+  },
+  {
+    "revision": "bdeeae434b270d487483",
+    "url": "/shizhang-li-homepage/static/js/runtime-main.205ba9d6.js"
+  },
+  {
+    "revision": "1cecca297c0c2a733264a1600e5f14f8",
+    "url": "/shizhang-li-homepage/static/media/einstein.1cecca29.jpg"
+  },
+  {
+    "revision": "c7b6f60fe83cb8b9dd5bd63ba39d4e40",
+    "url": "/shizhang-li-homepage/static/media/miscellaneous.c7b6f60f.jpg"
+  },
+  {
+    "revision": "b4d37f48255f7244d530f657f1fc9114",
+    "url": "/shizhang-li-homepage/static/media/monkey.b4d37f48.jpg"
+  },
+  {
+    "revision": "a658b0bf5cc2687f59173c6c628670cf",
+    "url": "/shizhang-li-homepage/static/media/seminar.a658b0bf.jpg"
+  },
+  {
+    "revision": "6818ea6cffc934d5a210d0bcbb4b50d2",
+    "url": "/shizhang-li-homepage/static/media/shizhangli.6818ea6c.jpg"
+  },
+  {
+    "revision": "90769d2ba14c2a10ee197aab8e33e77a",
+    "url": "/shizhang-li-homepage/static/media/take_notes.90769d2b.jpg"
+  },
+  {
+    "revision": "bf405493e6840b76df3cead0819dad07",
+    "url": "/shizhang-li-homepage/static/media/travel.bf405493.jpg"
+  }
+]);
